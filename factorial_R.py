af2a97dd2c13a96d9e10d0fdb31fa06cc549a54e
Alevv <134009912+Malejav18@users.noreply.github.com>
2025-02-11 16:01:12 -0500

    Update factorial_R.py

--- factorial_R.py
+++ factorial_R.py
@@ -21,8 +21,13 @@ t1=time.time()
 print("tiempo de ejecucion (500): ", t1-t0)
 
 '''
-Resultados: 
+Mac: 
 tiempo de ejecucion (5):  2.86102294921875e-06
 tiempo de ejecucion (50):  0.0001361370086669922
 tiempo de ejecucion (500):  0.0008399486541748047
+
+Windows:
+tiempo de ejecucion (5):  3.5762786865234375e-06
+tiempo de ejecucion (50):  3.218650817871094e-05
+tiempo de ejecucion (500):  0.0005970001220703125
 '''
